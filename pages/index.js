ddda469607f472c92a12bce16f704f2770fa0cda
rick <rickbhowmik4.com>
2025-03-20 01:06:04 +0530

    update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ useEffect(() => {
   const fetchHackathons = async () => {
     try {
       const API_URL = process.env.NODE_ENV === 'production'
-        ? 'https://your-actual-deployed-api.com/hackathons' // This should be your real API URL!
+        ? 'https://new-devfolio.vercel.app/' // This should be your real API URL!
         : 'http://localhost:5000/hackathons';
       
       const response = await fetch(API_URL);
